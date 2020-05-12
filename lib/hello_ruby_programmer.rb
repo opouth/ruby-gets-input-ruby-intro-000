0d885892859514b570d09a4eb39f97823833ca72
Ermite Rikudo <opouth@hotmail.fr>
2020-05-12 03:10:33 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,5 +1,5 @@
 puts "Please enter your name so that we can greet you more personally:"
-name = gets.chomp
+name = gets.strip
 def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
